26623b44f3a69eb74d0f5cb351c9c2e43a15cbe9
Amro Diab <adiab@hotmail.co.uk>
2017-10-09 01:03:47 +0100

    find non-dependant installed arch packages

--- bin/no_dep.sh
+++ bin/no_dep.sh
@@ -1,17 +1,10 @@
 #!/usr/bin/bash
 
-# From given list of arch packages, produce list of those which are not required
-# by any other packages
-# Author: Amro Diab
-# Date: 23/08/17
-
 in_file=$1
 out_file=$2
 for package in `cat $in_file`; do
 
-  if pacman -Qi $package |grep -q "Required By     : None"; then
-    echo $package >> $out_file
-  fi
+  pacman -Qi $package |grep -q "Required By     : None" && echo "no dep in $package" >> $out_file
 
 done
 
