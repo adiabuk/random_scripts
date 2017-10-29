0a0607b9ab4250a19f1b90301f931cb52d75339c
Amro Diab <adiab@hotmail.co.uk>
2017-10-29 14:02:46 +0000

    moved local bin files to seperate repo
    https://github.com/adiabuk/binfiles

--- bin/publicip.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Get public IP address
-
-set -eu
-set -o pipefail
-result=`wget -qO- http://ipecho.net/plain` || echo "No Address"
-echo $result | grep -qi html && echo "No Address" && exit 1
-echo $result && echo
--- bin/rename_pdf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in `find |grep _ocr_ocr`; do
-  renamed=`echo $i|sed 's/_ocr_ocr/_ocr/g'`
-  echo $i
-  echo $renamed
-  echo
-  mv $i $renamed
-done
